f51dbb12129124db8305adb19b77b00c51485b48
NicolaPiperis <nicolapiperis9999@gmail.com>
2023-05-22 17:23:08 +0200

    main style, task list and "addTask" problems

--- js/main.js
+++ js/main.js
@@ -1,14 +1,54 @@
-// Rifare l’esercizio della to do list.
-// Questa volta però ogni elemento della todo sarà un oggetto, formato da due proprietà:
+// Ogni elemento della todo sarà un oggetto, formato da due proprietà:
 // - text, una stringa che indica il testo del task
 // - done, un booleano (true/false) che indica se il task è stato fatto oppure no
+const {createApp} = Vue;
+createApp({
+    data(){
+        return{
+            newTask : "",
+            items : [
+                {
+                    text : "Fare il letto",
+                    done : true
+                },
+                {
+                    text : "Lavare il Bagno",
+                    done : false
+                },
+                {
+                    text : "Andare in palestra",
+                    done : true
+                },
+                {
+                    text : "Ritirare le analisi del sangue dall clinica",
+                    done : true
+                },
+                {
+                    text : "Studiare",
+                    done : false
+                }
+            ]
+        }
+    },
+    methods: {
+        addTask() {
+            this.items.push(this.newTask);
+        }
+    }
+
+}).mount("#app");
+
 // MILESTONE 1
 // Stampare all’interno di una lista HTML un item per ogni task.
 // Se la proprietà done è uguale a true, visualizzare il testo del task sbarrato.
+
 // MILESTONE 2
 // Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il task viene rimosso dalla lista.
+
 // MILESTONE 3
 // Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo task, che quindi viene aggiunto alla lista dei task esistenti.
+
+
 // Bonus:
 // 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il task alla lista
 // 2- cliccando sul testo dell’item, invertire il valore della proprietà done del task corrispondente (se done era uguale a false, impostare true e viceversa)
